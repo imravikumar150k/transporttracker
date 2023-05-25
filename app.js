4925fee71bccc366bab82027709eee3046f1cba2
Ravi Yadav <ravikumar150k@gmail.com>
2023-05-25 17:31:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-var dictionary = {"1":"https://maps.app.goo.gl/N6g4BkBCAGQMQJyj6", "2":"https://maps.app.goo.gl/N6g4BkBCAGQMQJyj6"}
+var dictionary = {"Cab-1":"https://maps.app.goo.gl/N6g4BkBCAGQMQJyj6", "2":"https://maps.app.goo.gl/N6g4BkBCAGQMQJyj6"}
